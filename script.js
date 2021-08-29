4a4ebdcd6c6ac0902100e75bac1ba92a9490f8e1
DmytriiV <89582247+DmytriiV@users.noreply.github.com>
2021-08-29 11:38:47 +0200

    upd

--- script.js
+++ script.js
@@ -4,4 +4,38 @@
 document.getElementsByClassName('menu_item')[0].addEventListener('click', function (event) {
     document.getElementsByClassName('content')[0].innerHTML = event.target.innerHTML;
 });
+document.getElementsByClassName('menu_item1')[0].addEventListener('click', function (event) {
+    document.getElementsByClassName('content')[0].innerHTML = event.target.innerHTML;
+});
+document.getElementsByClassName('menu_item2')[0].addEventListener('click', function (event) {
+    document.getElementsByClassName('content')[0].innerHTML = event.target.innerHTML;
+});
+document.getElementsByClassName('menu_item3')[0].addEventListener('click', function (event) {
+    document.getElementsByClassName('content')[0].innerHTML = event.target.innerHTML;
+});
+document.getElementsByClassName('menu_item4')[0].addEventListener('click', function (event) {
+    document.getElementsByClassName('content')[0].innerHTML = event.target.innerHTML;
+});
 
+document.getElementsByClassName('menu_item01')[0].addEventListener('click', function (event) {
+    document.getElementsByClassName('content')[0].innerHTML = event.target.innerHTML;
+});
+document.getElementsByClassName('menu_item02')[0].addEventListener('click', function (event) {
+    document.getElementsByClassName('content')[0].innerHTML = event.target.innerHTML;
+});
+document.getElementsByClassName('menu_item03')[0].addEventListener('click', function (event) {
+    document.getElementsByClassName('content')[0].innerHTML = event.target.innerHTML;
+});
+document.getElementsByClassName('menu_item04')[0].addEventListener('click', function (event) {
+    document.getElementsByClassName('content')[0].innerHTML = event.target.innerHTML;
+});
+document.getElementsByClassName('menu_item05')[0].addEventListener('click', function (event) {
+    document.getElementsByClassName('content')[0].innerHTML = event.target.innerHTML;
+
+});
+document.getElementsByClassName('menu_item06')[0].addEventListener('click', function (event) {
+    document.getElementsByClassName('content')[0].innerHTML = event.target.innerHTML;
+});
+document.getElementsByClassName('menu_item07')[0].addEventListener('click', function (event) {
+    document.getElementsByClassName('content')[0].innerHTML = event.target.innerHTML;
+});
